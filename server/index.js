e03d942d41ff3d791fc0575a23035d8ff2407ef3
bhimaesports <bhimaesports@gmail.com>
2026-06-24 09:11:08 +0530

    Fix CORS for Vercel

--- server/index.js
+++ server/index.js
@@ -40,10 +40,14 @@ app.use(helmet({
 }));
 
 app.use(cors({
-  origin: ['http://localhost:5173', 'http://localhost:3000', 'http://localhost:3001'],
+  origin: [
+    'http://localhost:5173',
+    'http://localhost:3000',
+    'http://localhost:3001',
+    'https://bhima-esports.vercel.app'
+  ],
   credentials: true,
 }));
-
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 
